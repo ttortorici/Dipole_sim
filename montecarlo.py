de37a4708257b0ecf202efb452bbc241bef654ff
Teddy Tort <etc.tortorici@gmail.com>
2023-03-21 08:49:18 -0600

    sim

--- montecarlo.py
+++ montecarlo.py
@@ -3,23 +3,24 @@ import matplotlib.pylab as plt
 import random
 import os
 
-
 # a = 1.1  # nm
 # dipole_strength = 0.08789  # electron charge - nm
 # eps_rel = 1.5
 
-eps0 = 0.0552713        # (electron charge)^2 / (eV - nm)
-boltzmann = 8.617e-5    # eV / K
+eps0 = 0.0552713  # (electron charge)^2 / (eV - nm)
+boltzmann = 8.617e-5  # eV / K
 
 
 class DipoleSim:
-    def __init__(self, a: float, rows: int, columns: int, temp0,
-                 dipole_strength: float, orientations_num: int, eps_rel: float):
+    def __init__(self, a: float, rows: int, columns: int, temp0, field0,
+                 dipole_strength: float, orientations_num: int, eps_rel: float, p=None):
         """
-
+        Monte Carlo
         :param a: lattice spacing in nm
         :param rows: number of rows of dipoles
         :param columns: number of columns of dipoles
+        :param temp0: initial temperature
+        :param field0: initial electric field
         :param dipole_strength: dipole_strength in (electron charge) * nm
         :param orientations_num: number of possible orientations (if zero, sets to 3)
         :param eps_rel: relative dielectric constant of surroundings
@@ -27,28 +28,82 @@ class DipoleSim:
         # set units
         self.k_units = 0.25 / (np.pi * eps0 * eps_rel)
         self.beta = 1. / (boltzmann * temp0)
+        self.E = field0
 
         self.orientations = self.create_ori_vec(orientations_num) * dipole_strength
         self.r = self.gen_dipoles(a, columns, rows)
+        self.rows = rows
+        self.columns = columns
         self.N = columns * rows
-        self.p = self.gen_orientations(self.N, 3)
+        if p is None:
+            self.p = self.gen_orientations(self.N, 3)
+        else:
+            self.p = p
         self.img_num = 0
+        self.accepted = 0
         self.energy = 0
+
         self.calculate_energy_per_dipole()
 
     def step(self):
+        """
+        One step of the Monte Carlo
+        :return:
+        """
         trial_dipole = random.randint(0, self.N - 1)
-        trial_e = self.orientations[random.randint(0, 2)]
-        if self.p[trial_dipole] != trial_e:
-            trial_energy =
+        trial_p = self.orientations[random.randint(0, 2)]
+        if not (self.p[trial_dipole][0] == trial_p[0]):
+            dr = self.calculate_distances(trial_dipole)
+            r_sq = np.sum(dr * dr, 1)
+            r_sq[r_sq == 0] = np.inf
+            p_dot_p = np.sum(trial_p * self.p, 1)
+            p_dot_r = np.sum(trial_p * dr, 1) * np.sum(self.p * dr, 1)
+            trial_energy = self.k_units * np.sum(p_dot_p / r_sq ** 1.5 - 3. * p_dot_r / r_sq ** 2.5) \
+                           - np.sum(self.E * trial_p)
+            if random.random() < np.exp(-self.beta * (trial_energy - self.energy)):
+                self.accepted += 1
+                self.p[trial_dipole] = trial_p
+                self.energy = trial_energy
+        return self.energy
 
-    def calculate_distances(self, dipole_ind):
+    def step_internal(self):
+        """
+        One step of the Monte Carlo
+        :return:
+        """
+        trial_dipole = random.randint(self.columns, self.N - 1 - self.columns)
+        while trial_dipole % self.columns in [0, self.columns - 1]:
+            trial_dipole = random.randint(self.columns, self.N - 1 - self.columns)
+        trial_p = self.orientations[random.randint(0, 2)]
+        if not (self.p[trial_dipole][0] == trial_p[0]):
+            dr = self.calculate_distances(trial_dipole)
+            r_sq = np.sum(dr * dr, 1)
+            r_sq[r_sq == 0] = np.inf
+            p_dot_p = np.sum(trial_p * self.p, 1)
+            p_dot_r = np.sum(trial_p * dr, 1) * np.sum(self.p * dr, 1)
+            trial_energy = self.k_units * np.sum(p_dot_p / r_sq ** 1.5 - 3. * p_dot_r / r_sq ** 2.5) \
+                           - np.sum(self.E * trial_p)
+            if random.random() < np.exp(-self.beta * (trial_energy - self.energy)):
+                self.accepted += 1
+                self.p[trial_dipole] = trial_p
+                self.energy = trial_energy
+        return self.energy
+
+    def calculate_distances(self, dipole_ind) -> np.ndarray:
+        """
+        Calculate distances between specified dipole and all others
+        :param dipole_ind: integer index value of dipole to calculate distances to
+        :return: array of distance values of each dipole from specified one
+        """
         dr = self.r - self.r[dipole_ind]
         return dr
 
-    def calculate_energy_of_dipole(self, dipole_ind):
-        """calculates energy of dipole "ind" ignoring the field
-        only takes into account neighbors"""
+    def calculate_energy_of_dipole(self, dipole_ind: int) -> float:
+        """
+        Calculates energy of one dipole ignoring the field only takes into account neighbors
+        :param dipole_ind: integer index value of dipole to calculate energy of.
+        :return: energy of one dipole
+        """
         dr = self.calculate_distances(dipole_ind)
         r_sq = np.sum(dr * dr, 1)
         r_sq[r_sq == 0] = np.inf
@@ -56,31 +111,64 @@ class DipoleSim:
         p_dot_r = np.sum(self.p[dipole_ind] * dr, 1) * np.sum(self.p * dr, 1)
         return self.k_units * np.sum(p_dot_p / r_sq ** 1.5 - 3. * p_dot_r / r_sq ** 2.5)
 
-    def calculate_energy_per_dipole(self):
-        """update the total energy of the system"""
-        self.energy = -sum(self.px * self.Ex + self.py * self.Ey)
+    def calculate_energy_per_dipole(self) -> float:
+        """
+        Update the total energy per dipole of the system
+        :return: total energy of the system
+        """
+        # self.energy = -sum(self.px * self.Ex + self.py * self.Ey)
+        self.energy = -np.sum(self.p * self.E)
         for ii in range(self.N):
             self.energy += self.calculate_energy_of_dipole(ii)
         self.energy /= 2. * self.N
         return self.energy
 
-    def change_temperature(self, temperature):
+    def change_temperature(self, temperature: float):
+        """
+        Change the current temperature of the system
+        :param temperature:
+        """
         self.beta = 1. / (boltzmann * temperature)
 
+    def change_electric_field(self, new_field: np.ndarray):
+        """
+        Change the value of the external electric field
+        :param new_field: 2 valued vector of electric field strength
+        """
+        self.E = new_field
+
     def get_temperature(self):
+        """
+        Get the current temperature
+        :return: current system temperature in K
+        """
         return 1. / (boltzmann * self.beta)
+
     @staticmethod
     def gen_dipoles(a: float, width: int, height: int) -> np.ndarray:
+        """
+        Generate the vectors of position for each dipole
+        :param a: spacing between dipoles in nm
+        :param width: number of columns
+        :param height: number of rows
+        :return: array of 2-vectors representing position of dipoles
+        """
         sqrt3half = np.sqrt(3) * 0.5
         x = 0.5 * a
         y = a * sqrt3half
         r = np.zeros((width * height, 2))
         for jj in range(height):
             start = jj * height
-            r[start:start+width, :] = np.stack((np.arange(width) * a + x * jj, np.ones(width) * y * jj), 1)
+            r[start:start + width, :] = np.stack((np.arange(width) * a + x * jj, np.ones(width) * y * jj), 1)
         return r
 
     def gen_orientations(self, dipole_num: int, orientation_num: int) -> np.ndarray:
+        """
+        Initialize dipole directions
+        :param dipole_num: number of dipoles
+        :param orientation_num: number of possible orientations a dipole can take
+        :return: array of 2-vectors repsenting dipole strength in x and y directions
+        """
         e = np.zeros((dipole_num, 2))
         stop = orientation_num - 1
         for ii in range(dipole_num):
@@ -89,6 +177,11 @@ class DipoleSim:
 
     @staticmethod
     def create_ori_vec(orientations_num):
+        """
+        Creates the basis vectors for possible directions
+        :param orientations_num: number of possible directions
+        :return: array of 2-long basis vectors
+        """
         if orientations_num:
             del_theta = 2 * np.pi / orientations_num
             orientations = np.zeros(orientations_num, 2)
@@ -100,6 +193,9 @@ class DipoleSim:
         return orientations
 
     def save_img(self):
+        """
+        save plot of current state
+        """
         plt.figure()
         arrow_vecs = self.p * 3
         arrow_starts = self.r - arrow_vecs * 0.5
@@ -108,9 +204,19 @@ class DipoleSim:
             plt.arrow(start[0], start[1], p[0], p[1], length_includes_head=True, head_width=0.1, head_length=0.1)
         plt.savefig(f"plots{os.sep}{self.img_num}.png", dpi=1000, format=None, metadata=None,
                     bbox_inches=None, pad_inches=0, facecolor='auto', edgecolor=None)
+        plt.close()
+        self.img_num += 1
 
 
 if __name__ == "__main__":
-    sim = DipoleSim(1.1, 30, 30, 100, 0.08789, 0, 1.5)
-
-
+    # sim = DipoleSim(1.1, 30, 30, 45, np.array([0, 0]), 0.08789, 0, 1.5)
+    p = np.loadtxt('dipoles_35K_field_5000000steps.txt')
+    sim = DipoleSim(1.1, 30, 30, 100, np.array([0, 0]), 0.08789, 0, 1.5, p)
+    # sim.save_img()
+    for ii in range(1):
+        for _ in range(5000000):
+            sim.step_internal()
+            # sim.step()
+        sim.save_img()
+        print(ii)
+    np.savetxt('dipoles_100K_ferro_5000000.txt', sim.p)
