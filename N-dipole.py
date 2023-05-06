aca7f747351256aa5e0b5bed9b1a1912638bc1e4
Teddy Tort <etc.tortorici@gmail.com>
2023-05-06 11:30:04 -0600

    a

--- N-dipole.py
+++ N-dipole.py
@@ -1,6 +1,6 @@
 import numpy as np
 import itertools
-from numba import jit
+import numba as nb
 
 a = 1.1  # nm
 dipole_strength = 0.08789  # electron charge - nm
@@ -17,6 +17,7 @@ k_un = 0.25 / (np.pi * eps0 * eps_rel)
 # ey = np.array([1, -0.5, -0.5]) * dipole_strength
 
 
+# @nb.njit(nb.float32[:])
 def gen_dipoles(nx, ny):
     sqrt3half = np.sqrt(3) * 0.5
     n = nx * ny
@@ -35,7 +36,8 @@ def gen_directions(number_of_directions):
     arg = 2 * np.pi * np.arange(number_of_directions) / number_of_directions
     ex = np.cos(arg)
     ey = np.sin(arg)
-    return np.array([ex]), np.array([ey])
+    # return np.array([ex]), np.array([ey])
+    return ex, ey
 
 
 def calculate_energy_vec(electric_field, nx, ny, nd):
@@ -47,9 +49,11 @@ def calculate_energy_vec(electric_field, nx, ny, nd):
     energy_vec = np.zeros(N)
     polarization_x_vec = np.zeros(N)
     polarization_y_vec = np.zeros(N)
-    for di in itertools.product(*([range(3)] * (nx * ny))):
-        px = np.array([[ex[d0], ex[d1], ex[d2], ex[d3], ex[d4], ex[d5], ex[d6]]])
-        py = np.array([[ey[d0], ey[d1], ey[d2], ey[d3], ey[d4], ey[d5], ey[d6]]])
+    for di in itertools.product(*([range(nd)] * (nx * ny))):
+        px = np.array([[ex[d] for d in di]])
+        py = np.array([[ey[d] for d in di]])
+        # px = np.array([[ex[d0], ex[d1], ex[d2], ex[d3], ex[d4], ex[d5], ex[d6]]])
+        # py = np.array([[ey[d0], ey[d1], ey[d2], ey[d3], ey[d4], ey[d5], ey[d6]]])
         polarization_x_vec[counter] = np.sum(px)
         polarization_y_vec[counter] = np.sum(py)
         energy_vec[counter] = calc_energy(electric_field[0], electric_field[1], px, py, rx, ry)
@@ -73,28 +77,42 @@ def calc_energy(external_field_x, external_field_y, px, py, rx, ry):
     return k_un * neg_energy - np.sum(external_field_x * px + external_field_y * py)
 
 
+def calc_ave_energy(betas, U_vec):
+    betas = np.array([betas])
+    U_vec = np.array([U_vec]).transpose()
+    boltzmann_factors = np.exp(np.matmul(-U_vec, betas))    # matrix of energies by betas
+    partition_function = np.sum(boltzmann_factors, axis=0)  # one for every beta
+    average_energy = np.sum(U_vec * boltzmann_factors, axis=0) / partition_function
+    # average_energy = np.zeros(pts)
+    #
+    # for ii, b in enumerate(betas):
+    #     z_vec = np.exp(-U_vec * b)
+    #     z_inv = 1 / np.sum(z_vec)
+    #
+    #     average_energy[ii] = z_inv * np.sum(U_vec * z_vec)
+    return average_energy
+
+
 if __name__ == "__main__":
     from time import perf_counter
     import matplotlib.pylab as plt
 
     pts = 1000
 
-    temperatures = np.linspace(0, 1000, pts)
+    temperatures = np.linspace(0, 100000, pts)
     temperatures[0] = 1e-6
     betas = 1 / (temperatures * boltzmann)
     E = np.array([0, 0])
 
     start = perf_counter()
-    U_vec = calculate_energy_vec(E, 3, 3, 3)
+    width = 4
+    height = 4
+    directions = 2
+    U_vec = calculate_energy_vec(E, width, height, directions)
     print(perf_counter() - start)
-    average_energy = np.zeros(pts)
 
-    for ii, b in enumerate(betas):
-        z_vec = np.exp(-U_vec * b)
-        z_inv = 1 / np.sum(z_vec)
-
-        average_energy[ii] = z_inv * np.sum(U_vec * z_vec)
-        # average_polarization = z_inv * np.sum(np.transpose(polarization_vec) * z_vec, 1)
+    average_energy = calc_ave_energy(betas, U_vec)
+    # average_polarization = z_inv * np.sum(np.transpose(polarization_vec) * z_vec, 1)
 
     plt.plot(temperatures, average_energy)
     plt.xlabel("Temperature (K)")
